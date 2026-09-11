e5a5065dcf6c98e95f053c7259b76895b55c1745
toemayto <austinshuuu@gmail.com>
2026-09-10 22:58:46 -0500

    Frequency Recognition

---
+++ cpp/phase1C_prac/main.cpp
@@ -0,0 +1,76 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <complex>
+#include <numbers>
+
+double SingleSine(double amplitude, double freq, int i, double f_s, double phase) {
+    return amplitude * std::sin(2 * M_PI * freq * (i / f_s - phase));
+}
+
+double MultiFreqSine(std::vector<double> &freqs, std::vector<double> &amplitudes,
+    std::vector<double> &phases, int num_signals, int i, double f_s) {
+        double sum = 0.0;
+        for (int j = 0; j < num_signals; j++) {
+            sum += SingleSine(amplitudes[j], freqs[j], i, f_s, phases[j]);
+        }
+        return sum;
+    }
+
+int main() {
+    const double f_s = 10000; //sample frequency
+    const double duration = 0.1; // signal duration
+    const int N = (int) (f_s * duration); // number of samples total we'll take over the signal duration
+    //const double amplitude = 1;
+    // const double freq = 1000;
+    std::cout << "Enter how many frequencies you want in the signal: ";
+    int num_signals;
+    std::cin >> num_signals;
+    // const double phase = 0;
+    std::vector<double> freqs(num_signals);
+    std::vector<double> amplitudes(num_signals);
+    std::vector<double> phases(num_signals);
+
+    for (int i = 0; i < num_signals; i++) {
+        std::cout << "Enter signal frequency, amplitude, and phase"
+        << "(in this case let it be the rightward shift of the component): ";
+        std::cin >> freqs[i] >> amplitudes[i] >> phases[i];
+    }
+
+    std::vector<double> samples(N);
+
+    for (int i = 0; i < N; i++) {
+        // samples[i] = SingleSine(amplitude,freq, i, f_s, phase);
+        samples[i] = MultiFreqSine(freqs, amplitudes,phases, num_signals, i, f_s);
+    }
+
+    const double delta_f = f_s / N;
+
+    std::vector<std::complex<double>> X(N); // Transform for each frequency bin k, where the index is k
+
+    std::vector<double> mag(N);
+
+    double mag_l = 0.0; // Largest magnitude
+    int index_l = 0; // Largest index
+
+    for (int i = 0; i < N; i++) {
+        X[i] = std::complex<double>(0, 0);
+        for (int j = 0; j < N; j++) {
+            X[i] += std::complex<double>(samples[j]* std::cos(-2 * M_PI * i * j / N),
+                samples[j]* std::sin(-2 * M_PI * i * j / N));
+        }
+        mag[i] = sqrt(std::pow(X[i].real(), 2) + std::pow(X[i].imag(), 2));
+        if (mag[i] > mag_l) {
+            mag_l = mag[i];
+            index_l = i;
+        }
+    }
+    std::cout << "Largest bin: " << index_l << std::endl;
+    std::cout << mag[index_l] << " " << mag[N - index_l] << std::endl;
+    double dft_freq;
+    if (index_l > N / 2) {
+        index_l -= N;
+    } 
+    dft_freq = index_l * f_s / N;
+    std::cout << dft_freq << " Hz" << std::endl;
+}
